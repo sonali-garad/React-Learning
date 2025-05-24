d8faf5a384a3b9a71329803d12e8200aa259a042
sonali-garad <sonaligarad1234@gmail.com>
2025-05-24 16:51:57 +0530

    function added

--- 01_React_Basics/src/App.jsx
+++ 01_React_Basics/src/App.jsx
@@ -1,8 +1,12 @@
-
+import First from "./First"
 function App() {
 
   return (
+    <>
    <h1> This is first React projet!!!</h1>
+   <First/>
+   
+   </>
   )
 }
 
---
+++ 01_React_Basics/src/First.jsx
@@ -0,0 +1,8 @@
+function First()
+{
+    return (
+        <h1> Navigation from function </h1>
+
+    )
+}
+export default First
