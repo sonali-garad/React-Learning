7afe4fe1af74e4a85e4a6a0db97adada108ba7be
sonali garad <sgarad@academain.com>
2025-08-06 23:37:57 +0530

    map + loop

--- 06_Basic_Concepts/src/App.jsx
+++ 06_Basic_Concepts/src/App.jsx
@@ -5,10 +5,52 @@ import './App.css'
 
 function App() {
   
-  return (
-    <>
-      <h1> Hello</h1>
-    </>
+  const students = ['Sonali' , 'rick', 'john' , 'tylor'];
+
+  const Students1 =[{name :"ryan" , email: "ryan@gmail.com", contact: "8990"},
+    {name: "iyan" , email: "iyan@gmail.com", contact:"8239"}
+  ]
+  //Using Map
+  students.map((item) =>{
+    console.log(item)
+  })
+
+  //Using loop 
+  for(let i =0; i<students.length ; i++)
+  {
+console.log(students[i]);
+  }
+//loops are not allowed in the return statement
+  return(
+<>
+<h1>Handle array with List</h1>
+ {/* {
+  students.map((data)=>
+<h4 >{data}</h4>
+ )
+} */}
+
+{
+  <table border= "1px" cellPadding="2px">
+    <tr>
+  <td>Name</td>
+  <td>Email</td>
+  <td> Contact</td>
+
+    </tr>
+  {
+   
+  Students1.map((item)=>
+  <tr>
+    <td>{item.name}</td>
+  <td>{item.email}</td>
+  <td>{item.contact}</td>
+  </tr>
+  )   
+}
+  </table>
+}
+</>
   )
 }
 
