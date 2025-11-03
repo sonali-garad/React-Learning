7e5bd1e4d09fe6d93e7dccd1029662caabbbab18
sonali garad <sgarad@academain.com>
2025-11-03 23:54:31 +0530

    Data Display

--- 07_Axios_Use/src/App.jsx
+++ 07_Axios_Use/src/App.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 const App= () =>
 {
   const [data, setData] = useState([]);
-  
+
  //response.data ->  
 // body :"quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
 // id :1
@@ -15,7 +15,7 @@ const getUserData = async () =>
   try{
     const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
     setData(response)
-    console.log(response)
+    // console.log(response)
   }
   catch(e)
   {
@@ -25,10 +25,17 @@ const getUserData = async () =>
 getUserData();
 },
 [])
-
+console.log("whole data ",data?.data)
+console.log("response data",data?.data?.[0]?.id) //added optional chaining to prevent from code crashout cause at start we didn't have any value in the object without this chaining code would crash.
 
   return (
-    <h1> hello </h1>
+    <>
+    <h1 style={{color:"blue", textAlign:"center"}}>SAMPLE USER DATA DISPLAY FROM API </h1>
+    <h1 style ={{color: "red"}}> UserID : {data?.data?.[0]?.id}</h1>
+    <h1 style={{color: "green"}}>Title: {data?.data?.[0]?.title}</h1>
+    <h1 style= {{color: "violet"}}>Body: {data?.data?.[0]?.body}</h1>
+    </>
+    
   )
 }
 export default App
