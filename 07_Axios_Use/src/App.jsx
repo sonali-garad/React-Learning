bcf390eff8f815021abd04be320200d5b7bd3acd
sonali garad <sonaligarad1234@gmail.com>
2025-11-04 23:16:21 +0530

    Comment removed

--- 07_Axios_Use/src/App.jsx
+++ 07_Axios_Use/src/App.jsx
@@ -4,11 +4,7 @@ const App= () =>
 {
   const [data, setData] = useState([]);
 
- //response.data ->  
-// body :"quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-// id :1
-// title :"sunt aut facere repellat provident occaecati excepturi optio reprehenderit"
-// userId :1
+ 
 useEffect(()=>{
 const getUserData = async () =>
 {
