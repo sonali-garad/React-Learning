c18c1f0602e43487c3e0116a6c181aa041a2c670
sonali garad <sonaligarad1234@gmail.com>
2025-12-21 23:32:56 +0530

    Button added

--- 09_Snowfall_Lib_use/src/App.jsx
+++ 09_Snowfall_Lib_use/src/App.jsx
@@ -6,17 +6,38 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const[snow, setSnow] = useState(false)
 
   return (
+    
     <>
     <div style={{ height: "10vh", width: "80vh"}}>
-    <Snowfall 
+      {/* we are rendering the snow when snow state is true only - else we are not showing snow on screen  */}
+      {snow &&(
+    < Snowfall 
     color='#FFFAFA'
     snowflakeCount={400}
     radius={[5,0]}
     wind={[1,4]}
+
     />
+      )}
     <h1 style={{background: "beige", color:"black"}}> Let it snow</h1>
+    <div >
+    <button 
+    style={{background:"white", width: "140px", height:"40px", color:"black"}}
+    onClick={() => {setSnow(true)}}
+    > Start Snow  </button>
+
+   <button 
+      style={{background:"white", width: "140px", height:"40px", margin: "10px", color:"black"}}
+    text="hi"
+    onClick={()=> {setSnow(false)}}
+ >
+  Stop Snow 
+ </button>
+
+    </div>
     </div>
     </>
   )
