e8529c779b332c30f158516ae06b8a329c292b98
sonali garad <sonaligarad1234@gmail.com>
2026-01-06 23:39:22 +0530

    Consumer

--- 12_Context_API/src/components/ChildC.jsx
+++ 12_Context_API/src/components/ChildC.jsx
@@ -1,8 +1,12 @@
+import {UserContext} from "../App"
+import { useContext } from "react";
 const ChildC = () =>
 {
+    const context = useContext(UserContext)
+    console.log(context)
     return (
         <>
-        <p>Name is :</p>
+        <p>Name is : {context.name}</p>
         </>
     )
 }
