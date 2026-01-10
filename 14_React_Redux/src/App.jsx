dca8c376e45d677c8d5a3ec8505c4b12b304df4b
sonali garad <sonaligarad1234@gmail.com>
2026-01-10 23:09:59 +0530

    store config + state management

--- 14_React_Redux/src/App.jsx
+++ 14_React_Redux/src/App.jsx
@@ -1,9 +1,69 @@
+import "./App.css";
+import { store } from "./redux/Store/Store";
+import {
+  increment,
+  decrement,
+  reset,
+  randomAddition,
+} from "./redux/Slices/CounterSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
 function App() {
-  return(
-  <>
-  <h1>Hello</h1>
-  </>
-  )
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+  const [localCount, setLocalCount] = useState(0);
+  console.log("count", count);
+  console.log("localCount", localCount);
+
+  const incrementCount = () => {
+    dispatch(increment());
+  };
+
+  const decrementCount = () => {
+    dispatch(decrement());
+  };
+
+  const resetCount = () => {
+    dispatch(reset());
+  };
+
+  const increaseCountBy = () => {
+    dispatch(randomAddition(localCount));
+  };
+
+  return (
+    <>
+      <div className="container">
+        <h1 id="header">Redux State Management</h1>
+
+        <button className="button-con" onClick={incrementCount}>
+          Increment
+        </button>
+
+        <button className="button-con" onClick={decrementCount}>
+          {" "}
+          Decrement
+        </button>
+
+        <button className="button-con" onClick={resetCount}>
+          Reset
+        </button>
+
+        <h3>Count : {count}</h3>
+        <button style={{ margin: "3px" }} onClick={increaseCountBy}>
+          Increase Counter by
+        </button>
+
+        <input
+          type="number"
+          onChange={(e) => {
+            setLocalCount(e.target.value);
+          }}
+          
+        />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
